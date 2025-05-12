84c5769fbdd4fafd8a2369cbe0db9576ccda154d
Nicole <117069578+Ganglittle@users.noreply.github.com>
2025-05-12 22:34:56 +0300

    Update code-checkpoint.py

--- Assignment_6/.ipynb_checkpoints/code-checkpoint.py
+++ Assignment_6/.ipynb_checkpoints/code-checkpoint.py
@@ -3,3 +3,14 @@ df = pd.read_csv('todays_data.csv')
 
 print(df.head())
 
+df.info()
+
+print(df.isnull().sum())
+df = df.dropna()
+
+# Part 2
+print(df.describe())
+
+# Part 3
+import matplotlib.pyplot as plt
+df = pd.read_csv('todays_data.csv')
